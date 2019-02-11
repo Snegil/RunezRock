95ef1d88c631e2a611c212aa9da2fe0e3c84fbde
Snegil <144wallfin@gmail.com>
2019-02-11 22:23:16 +0100

    Flyttade Runezrock ifrån php-project till egen repository

---
+++ kontakt.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="author" content="Snegil">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8" lang="sv">
+		<title>
+			Runez Rock kontakt
+		</title>
+		<link rel='icon' href='favicon.ico' type='image/x-icon'/ >
+		<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="scripts.js"></script>
+	</head>
+	<body>
+				<div id="container">
+						<div id="wholepage">
+								<div id="logotyp">
+									<a href="medlemmar.php">
+									<i class="fas fa-angle-double-left button" title="Medlemmar"></i>
+									</a>
+										<a href="index.php">
+										<img class="logoimage" src="Images/runezrocklogoedit.png">
+										</a>
+									<a href="kontakt.php">
+									<i class="fas fa-angle-double-right button" title="Kontakt"></i>
+									</a>
+								</div>
+										<img id="bandimage" src="Images/runezrockband2.jpg">	
+													
+										<!-- <div id="infobar">
+											<a target="_blank" href="https://www.facebook.com/Runez-Rock-109810822368941/">
+											<img id="flogo" alt="Runez Rock finns på facebook!" src="Images/flogo2.png">
+											</a>
+											<iframe id="spotifyI" src="https://open.spotify.com/embed/track/40roys5JxjibSvp27c9ts6" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+										</div> -->
+										
+								<p id="about">
+										<div id="kontaktcontainer">
+											<div id="containerkontakt"
+												<div id="kontaktimgcontainer">
+													<img id="kontaktimg" src="Images/Profilbild/PelleEDIT2.jpg">
+												</div>
+												<span id="kontakttext"> 
+													<h3 id="h3kontakt">Pär Wallin</h3>
+													<a href="https://m.me/109810822368941?fbclid=IwAR3281zfYwgEZI21C7eAMUzB9eoPDa1amhekHLjfJVa8fXoKnqFwm71Jp7A" target="_blank">
+														<img id="mlogo" src="Images/mlogo.png">
+													</a>
+												</span>
+											 </div>
+										</div>
+								</p>
+									
+						</div>
+				</div>
+	</body>	
+</html>
---
+++ medlemmar.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta name="author" content="Snegil">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8" lang="sv">
+		<title>
+			Runez Rock medlemmar
+		</title>
+		<link rel='icon' href='favicon.ico' type='image/x-icon'/ >
+		<link rel="stylesheet" type="text/css" href="stylesheet.css" />
+		<link rel="stylesheet" type="text/css" href="https://csshake.surge.sh/csshake.css" />
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+		<script src="scripts.js"></script>
+	</head>
+	
+	<body>
+				<div id="container">
+						<div id="wholepage">
+								<div id="logotyp">
+									<a href="medlemmar.php">
+									<i class="fas fa-angle-double-left button" title="Medlemmar"></i>
+									</a>
+										<a href="index.php">
+										<img class="logoimage" src="Images/runezrocklogoedit.png">
+										</a>
+									<a href="kontakt.php">
+									<i class="fas fa-angle-double-right button" title="Kontakt"></i>
+									</a>
+								</div>
+										<img id="bandimage" src="Images/runezrockband2.jpg">	
+													
+										<!-- <div id="infobar">
+											<a target="_blank" href="https://www.facebook.com/Runez-Rock-109810822368941/">
+											<img id="flogo" alt="Runez Rock finns på facebook!" src="Images/flogo2.png">
+											</a>
+											<iframe id="spotifyI" src="https://open.spotify.com/embed/track/40roys5JxjibSvp27c9ts6" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+										</div>
+										 -->
+								<p id="About">
+									<div class="center">
+											<div class="profilContainer">			
+												<div class="profilimgcontainerL">
+													<span class="imgL">
+													<img class="shake-slow" src="Images/Profilbild/PerTEDIT.jpg" width="150px" height="150px">
+													</span>
+													<span class="profilTextL">
+														<h3>Per Thilly</h3>
+														<span class="profiltextdiv">
+														Sång <br />
+														Text text text text text lorem lipsum blablabla
+														</span>
+													</span>
+												</div>
+											</div>
+											
+											<div class="profilContainer">			
+												<div class="profilimgcontainerR">
+													<span class="imgR">
+													<img class="shake-slow" src="Images/Profilbild/SonnyEDIT.jpg" width="150px" height="150px">
+													</span>
+													<span class="profilTextR">
+														<h3>Sonny Olausson</h3>
+														<span class="profiltextdiv">
+														Gitarr <br />
+														Text text text text text lorem lipsum blablabla
+														</span>
+													</span>
+												</div>
+											</div>
+											
+											<div class="profilContainer">			
+												<div class="profilimgcontainerL">
+													<span class="imgL">
+													<img class="shake-slow" src="Images/Profilbild/PelleEDIT.jpg" width="150px" height="150px">
+													</span>
+													<span class="profilTextL">
+														<h3>Pär Wallin</h3>
+														<span class="profiltextdiv">
+														Gitarr <br />
+														Text text text text text lorem lipsum blablabla
+														</span>
+													</span>
+												</div>
+											</div>
+											
+											<div class="profilContainer">			
+												<div class="profilimgcontainerR">
+													<span class="imgR">
+													<img class="shake-slow" src="Images/Profilbild/GoranEDIT.jpg" width="150px" height="150px">
+													</span>
+													<span class="profilTextR">
+														<h3>Göran Lidh</h3>
+														<span class="profiltextdiv">
+														Trummor <br />
+														Text text text text text lorem lipsum blablabla
+														</span>
+													</span>
+												</div>
+											</div>
+											
+											<div class="profilContainer">			
+												<div class="profilimgcontainerL">
+													<span class="imgL">
+													<img class="shake-slow" src="Images/Profilbild/KennethEDIT.jpg" width="150px" height="150px">
+													</span>
+													<span class="profilTextL">
+														<h3>Kenneth "Tjabbe" Carlsson</h3>
+														<span class="profiltextdiv">
+														Keyboards <br />
+														Text text text text text lorem lipsum blablabla
+														</span>
+													</span>
+												</div>
+											</div>
+											
+											<div class="profilContainer">			
+												<div class="profilimgcontainerR">
+													<span class="imgR">
+													<img class="shake-slow" src="Images/Profilbild/RasmusEDIT2.jpg" width="150px" height="150px">
+													</span>
+													<span class="profilTextR">
+														<h3>Rasmus Thilly</h3>
+														<span class="profiltextdiv">
+														Bas <br />
+														Text text text text text lorem lipsum blablabla
+														</span>
+													</span>
+												</div>
+											</div>
+										</div>
+									</div>
+								</p>
+									
+						</div>
+				</div>
+	</body>	
+</html>
